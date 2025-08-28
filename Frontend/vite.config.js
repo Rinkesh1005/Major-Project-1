20f2efbfd09afcbf2c97d6301e849704d7fb2f04
Rinkesh Bhai Patel <rinkeshbhaipatel@Rinkeshs-MacBook-Pro.local>
2025-08-28 18:55:15 +0530

    Added favicon and meta tags

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -12,9 +12,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    __DEFINES__: {}, 
-    __HMR_CONFIG_NAME__: undefined,
-    __BASE__: '"/"',
-  },
 })
