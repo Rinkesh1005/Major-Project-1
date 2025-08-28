b7b34e88c42254ab7eab89d1c827d8e3b3ced820
Rinkesh Bhai Patel <rinkeshbhaipatel@Rinkeshs-MacBook-Pro.local>
2025-08-28 19:33:26 +0530

    Added favicon and meta tags

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,29 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Avoid __DEFINES__ not defined error
-    __DEFINES__: {},
-  },
-  build: {
-    target: 'esnext',
-    sourcemap: false,
-  },
-  server: {
-    fs: {
-      strict: false, // Helps with Vercel deployment sometimes
-    },
-  },
 })
+
+
